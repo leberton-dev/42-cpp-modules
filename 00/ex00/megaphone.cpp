525e91235eca999a53a8c074033f1dd80bad3628
TheLeBerton <theleberton@gmail.com>
2026-06-04 15:23:09 +0200

    FIX handle empty string in _handle_params #25

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 
-static int _handle_params (int argc);
+static int _handle_params (int argc, char** argv);
 static void _print_str_uppercase (char* str);
 
 int main (int argc, char** argv) {
     int i;
 
-    if (_handle_params (argc) == 1)
+    if (_handle_params (argc, argv) == 1)
         return (0);
     i = 1;
     while (i < argc) {
@@ -17,8 +17,16 @@ int main (int argc, char** argv) {
     return (0);
 }
 
-static int _handle_params (int argc) {
-    if (argc == 1) {
+static size_t _strlen (char* str) {
+    size_t i = 0;
+    while (str[i]) {
+        i++;
+    }
+    return (i);
+}
+
+static int _handle_params (int argc, char** argv) {
+    if (argc == 1 || _strlen (argv[1]) == 0) {
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
         return (1);
     }
