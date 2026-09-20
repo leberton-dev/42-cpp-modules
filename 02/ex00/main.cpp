31d886a5f9ffacee7ef00a8445cc1e462991b846
Leonardo Bertonasco <leberton@c2r7p5.42vienna.com>
2026-09-20 11:08:26 +0200

    ADD more tests to main 02 ex00

--- 02/ex00/main.cpp
+++ 02/ex00/main.cpp
@@ -1,13 +1,45 @@
 #include "Fixed.hpp"
 #include <iostream>
 
+static void testGetRawBits() {
+	std::cout << "[ TEST GET RAW BITS ]" << std::endl;
+	Fixed a;
+
+	std::cout << a.getRawBits() << std::endl;
+}
+
+static void testSetRawBits() {
+	std::cout << "[ TEST SET RAW BITS ]" << std::endl;
+	Fixed a;
+
+	a.setRawBits(10);
+	std::cout << a.getRawBits() << std::endl;
+}
+
+static void testDefaultConstructor() {
+	std::cout << "[ TEST DEFAULT CONSTRUCTOR ]" << std::endl;
+
+	Fixed a;
+}
+
+static void testCopyConstructor() {
+	std::cout << "[ TEST COPY CONSTRUCTOR ]" << std::endl;
+
+	Fixed a;
+	Fixed b = Fixed(a);
+}
+
+static void testCopyAssignementOperator() {
+	std::cout << "[ TEST COPY ASSIGNEMENT CONSTRUCTOR ]" << std::endl;
+	Fixed a;
+	Fixed b = a;
+}
+
 int main (void) {
-    Fixed a;
-    Fixed b (a);
-    Fixed c;
-    c = b;
-    std::cout << a.getRawBits () << std::endl;
-    std::cout << b.getRawBits () << std::endl;
-    std::cout << c.getRawBits () << std::endl;
+	testGetRawBits();
+	testSetRawBits();
+	testDefaultConstructor();
+	testCopyConstructor();
+	testCopyAssignementOperator();
     return 0;
 }
